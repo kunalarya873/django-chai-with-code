c64456c108aa4f354dbcf4d02d6844e3916471f6
kunalarya873 <kunalarya873@gmail.com>
2024-06-05 15:54:40 +0530

    Tailwind attaching

--- chaiaurDjango/chaiaurDjango/settings.py
+++ chaiaurDjango/chaiaurDjango/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chai',
     'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
