31b5f1e7b44825532bbb6a5ff701180b0cfbe97e
kunalarya873 <kunalarya873@gmail.com>
2024-06-24 14:30:50 +0530

    Adding full project

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-import requests
 from .models import Tweet
-from .forms import TweetForm
+from .forms import TweetForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 def tweet_list(request):
     tweets = Tweet.objects.all().order_by('created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
-
+@login_required
 def tweet_create(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
@@ -18,6 +19,8 @@ def tweet_create(request):
     else:
         form= TweetForm()
     return render(request, 'tweet_form.html', {'form': form})
+
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user= request.user)
     if request.method=="POST":
@@ -32,10 +35,25 @@ def tweet_edit(request, tweet_id):
         form= TweetForm(instance=Tweet)
     return render(request, 'tweet_form.html', {'form': form})
 
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method=='POST':
         tweet.delete()
         return redirect('tweet_list')
     return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('tweet_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html',
+                  {'form': form})
 
